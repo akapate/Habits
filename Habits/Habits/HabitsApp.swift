f12da1a983a1b526b205141be423bc629b93d12b
Pavel Petrenko <59023939+akapate@users.noreply.github.com>
2025-03-15 12:23:59 +0300

    Created Xcode project

---
+++ Habits/Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Паша Петренко on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
